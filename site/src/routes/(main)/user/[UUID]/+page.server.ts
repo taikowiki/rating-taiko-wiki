52d001f31a3e6c942da240b3064d8e5bf60ebcdb
hotsixman <hotsixman05@gmail.com>
2025-11-16 22:12:08 +0900

    fix: profile, version: 0.0.4

--- site/src/routes/(main)/user/[UUID]/+page.server.ts
+++ site/src/routes/(main)/user/[UUID]/+page.server.ts
@@ -2,15 +2,11 @@ import type { User } from '$lib/module/user/index.js';
 import { userDBController } from '$lib/module/user/server.js';
 import { error } from '@sveltejs/kit';
 
-const profile: User.Profile = {
-    nickname: '핫식스맨',
-    UUID: '',
-    bio: ''
-};
-
 export async function load({ params }) {
     const UUID = params.UUID;
 
+    const profile = await userDBController.getProfile(UUID);
+    if(!profile) throw error(404);
     const taikoProfile = await userDBController.getTaikoProfile(UUID);
     if (!taikoProfile) throw error(404);
     const ratingData = await userDBController.getRatingData(UUID);
