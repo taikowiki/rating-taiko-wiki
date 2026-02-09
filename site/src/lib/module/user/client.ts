3b6b206e506a00c15c8bb3afc0a463256bd34842
hotsixman <hotsixman05@gmail.com>
2026-02-09 21:17:44 +0900

    fix: 데이터 삭제 요청 함수

--- site/src/lib/module/user/client.ts
+++ site/src/lib/module/user/client.ts
@@ -44,7 +44,7 @@ export namespace userRequestor {
     }
 
     export async function deleteData(): Promise<RequestResult<void>> {
-        const response = await fetch('/api/priuvate/delete-profile', {
+        const response = await fetch('/api/private/delete-profile', {
             method: 'post',
             body: JSON.stringify({
                 agree: true
